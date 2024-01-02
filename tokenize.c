29b4c01210e266d445ed02abf0575e940a3ae4a0
Kirby <maxman306@gmail.com>
2024-01-02 17:56:41 +0900

    feat: :sparkles: Added Simple Tokenizing Sequence
    Now this program can tokenize by quotes and spaces

--- tokenize.c
+++ tokenize.c
@@ -6,7 +6,7 @@
 /*   By: junkim2 <junkim2@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/01 17:30:30 by junkim2           #+#    #+#             */
-/*   Updated: 2024/01/01 20:45:06 by junkim2          ###   ########.fr       */
+/*   Updated: 2024/01/02 17:53:15 by junkim2          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,12 @@ char	is_operator(char c)
 	i = 0;
 	while (i < 9)
 	{
-		// 한자리 연산자의 경우
 		if (ft_strlen(control_operator[i]) == 1)
 		{
 			if (control_operator[i][0] == c)
 				return (1);
 		}
-		else	// 2자리 이상의 연산자의 경우
+		else
 		{
 			if (ft_strchr(control_operator[i], c) != 0)
 				return (1);
@@ -36,22 +35,92 @@ char	is_operator(char c)
 	return (0);
 }
 
+int	is_comb_operator(char c1, char c2)
+{
+	const char	*control_operator_double[2] = {"<<", ">>"};
+
+	int	i;
+	
+	i = 0;
+	while (i < 2)
+	{
+		if (c1 == control_operator_double[i][0] && c2 == control_operator_double[i][1] || \
+			c2 == control_operator_double[i][1] && c2 == control_operator_double[i][0])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+void	make_token(t_list **list, char *str, int start, int end)
+{
+	t_list	*token;
+	char	*substr;
+
+	substr = ft_substr(str, start, end - start);
+	if (str == NULL)
+		exit(EXIT_FAILURE);
+	token = ft_lstnew((void *)substr);
+	if (token == NULL)
+		exit(EXIT_FAILURE);
+	ft_lstadd_back(list, token);
+}
+
+void	tokenize(t_list **list, char *str)
+{
+	int	start;
+	int	end;
+
+	start = 0;
+	end = 0;
+	end++;
+	while (str[end])
+	{
+		if (str[end] == '\'')
+		{
+			end++;
+			while (str[end] && str[end] != '\'')
+				end++;
+			make_token(list, str, start + 1, end);
+			start = end + 1;
+		}
+		else if (str[end] == '\"')
+		{
+			end++;
+			while (str[end] && str[end] != '\"')
+				end++;
+			make_token(list, str, start + 1, end);
+			start = end + 1;
+		}
+		else if (str[end] == ' ')
+		{
+			if (str[end - 1] != '\'' && str[end - 1] != '\"' && str[end - 1] != ' ')
+				make_token(list, str, start, end);
+			start = end + 1;
+		}
+		end++;
+	}
+	make_token(list, str, start, end);
+}
+
 int	main(int argc, char **argv)
 {
-	int	i;
+	int		i;
+	t_list	*token_list;
+	t_list	*cur;
 
 	i = 0;
 	if (argc < 2)
 		return (0);
-	printf("%s\n", argv[1]);
-	while (argv[1][i])
+	tokenize(&token_list, argv[1]);
+	cur = token_list;
+	i = 0;
+	while (cur)
 	{
-		printf("%c is ", argv[1][i]);
-		if (is_operator(argv[1][i]))
-			printf("operator\n");
-		else
-			printf("not operator\n");
+		printf("[%s] ", (char *)cur->content);
 		i++;
+		cur = cur->next;
 	}
+	printf("\n");
 	return (0);
 }
