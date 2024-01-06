00b5435b6e95640d5885c41fb33811d1430a8baf
Kirby <maxman306@gmail.com>
2024-01-06 19:48:59 +0900

    fix: :bug: Changed quoting method
    Now we can parse every ", ' and \", \'

--- parse/error.c
+++ parse/error.c
@@ -1,31 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   util.c                                             :+:      :+:    :+:   */
+/*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: junkim2 <junkim2@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/05 21:31:09 by junkim2           #+#    #+#             */
-/*   Updated: 2024/01/06 18:43:52 by junkim2          ###   ########.fr       */
+/*   Created: 2024/01/06 17:23:11 by junkim2           #+#    #+#             */
+/*   Updated: 2024/01/06 18:48:37 by junkim2          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	is_quote(char *str, int	idx)
+void	*quote_err(char c)
 {
-	if (idx == 0)
-	{
-		if (str[idx] == '\'' || str[idx] == '\"')
-			return (str[idx]);
-		else
-			return (0);
-	}
-	else
-	{
-		if ((str[idx] == '\"' || str[idx] == '\'') && str[idx - 1] != '\\')
-			return (str[idx]);
-		else
-			return (0);
-	}
+	write(2, "minishell: unexpected EOF while looking for matching `", 55);
+	write(2, &c, 1);
+	write(2, "\'\nminishell: syntax error: unexpected end of file\n", 51);
+	return (NULL);
 }
