4102b374d05c3c69f4a9a5da62c4b270aa262766
minjacho42 <minjacho@student.42seoul.kr>
2024-01-07 16:51:33 +0900

    fix: :zap: tree, start dir leaks handled
    tree, start dir leaks handled

--- exec/exec_heredoc.c
+++ exec/exec_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: minjacho <minjacho@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 19:04:20 by minjacho          #+#    #+#             */
-/*   Updated: 2024/01/07 15:48:18 by minjacho         ###   ########.fr       */
+/*   Updated: 2024/01/07 16:36:07 by minjacho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -104,4 +104,5 @@ void	unlink_tmpfile(int cnt, char *start_dir)
 		idx++;
 	}
 	free(heredoc_prefix);
+	free(start_dir);
 }
